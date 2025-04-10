2528105122e337de488144a234edc1021f1cfc55
Pedro Augusto <162388916+pedroaalexandre@users.noreply.github.com>
2025-04-10 20:30:54 -0300

    Update main.js
    Update the link of dados.JSON

--- requisicao_assincrona/Lista03_A/js/main.js
+++ requisicao_assincrona/Lista03_A/js/main.js
@@ -1,7 +1,7 @@
 import requests from "./request.js";
 
 window.addEventListener("DOMContentLoaded", function() {
-    requests.request1("https://pedroaalexandre.github.io/PFWJ/Lista03_A/dados.JSON");
-    requests.request2("https://pedroaalexandre.github.io/PFWJ/Lista03_A/dados.JSON");
+    requests.request1("https://pedroaalexandre.github.io/PFWJ/requisicao_assincrona/Lista03_A/dados.JSON");
+    requests.request2("https://pedroaalexandre.github.io/PFWJ/requisicao_assincrona/Lista03_A/dados.JSON");
     
 });
