274eaf7d570ae560bfd36f31abce0a6d20d055c5
Pedro Augusto <162388916+pedroaalexandre@users.noreply.github.com>
2025-04-10 20:27:15 -0300

    Delete requisicao_assincrona directory

--- requisicao_assincrona/Lista03_A/js/main.js
+++
@@ -1,7 +0,0 @@
-import requests from "./request.js";
-
-window.addEventListener("DOMContentLoaded", function() {
-    requests.request1("https://pedroaalexandre.github.io/PFWJ/dados.JSON");
-    requests.request2("https://pedroaalexandre.github.io/PFWJ/dados.JSON");
-    
-});
