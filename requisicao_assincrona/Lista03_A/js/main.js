4a5cc27db353cc668f9c3795dda8594de00466d5
Pedro Augusto <162388916+pedroaalexandre@users.noreply.github.com>
2025-04-10 20:26:44 -0300

    Add files via upload

---
+++ requisicao_assincrona/Lista03_A/js/main.js
@@ -0,0 +1,7 @@
+import requests from "./request.js";
+
+window.addEventListener("DOMContentLoaded", function() {
+    requests.request1("https://pedroaalexandre.github.io/PFWJ/dados.JSON");
+    requests.request2("https://pedroaalexandre.github.io/PFWJ/dados.JSON");
+    
+});
